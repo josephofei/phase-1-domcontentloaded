dcce96e98fc662f16d73429219ac08d156efb5e8
Joseph Ofei <josephofei24@gmail.com>
2023-04-06 11:34:02 -0400

    DomContentLoaded complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    let select = document.querySelector('p')
+    select.innerHTML = "This is really cool!"
+    //console.log(select)
+});
+
